865bed3da9ffd5d502a7ba9c07784e9b84591c18
PicInfiniti <picinfiniti@gmail.com>
2026-02-25 09:58:28 -0500

    fix pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        pay: resolve(__dirname, 'pages/pay.html'),
-      }
-    }
-  }
+        main: resolve(__dirname, "index.html"),
+        pay: resolve(__dirname, "pages/pay.html"),
+        logarithm: resolve(__dirname, "pages/logarithm.html"),
+      },
+    },
+  },
 });
